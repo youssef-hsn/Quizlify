47bb5ebdc48836817f701fe5ba1df4577bc92597
youssef <youssefhsn16@gmail.com>
2024-07-23 13:44:30 +0300

    ⚙️ Add Config file

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+    $database_host = 'localhost';
+    $database_name = 'quizlify';
+    $database_user = 'root';
+    $database_password = '';
+    $conn = mysqli_connect($database_host, $database_user, $database_password, $database_name);
+?>
