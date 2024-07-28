e9a39e11dd058eb7d0abf86be5143ff5aa5c7cef
youssef <youssefhsn16@gmail.com>
2024-07-28 12:44:04 +0300

    🔎 Fix getQuizData to dynamically change with according to the request

--- api/getQuizData.php
+++ api/getQuizData.php
@@ -12,13 +12,35 @@ try {
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $quiz_id = 1; // Assuming you want to fetch quiz with id 1
+    if (!isset($_GET['id'])) {
+        http_response_code(400);
+        echo json_encode([
+            'error' => 'Quiz ID not provided',
+            'description' => 'You need to provide a quiz "id" attribure'
+        ]);
+        exit();
+    } else if (!is_numeric($_GET['id'])) {
+        http_response_code(400);
+        echo json_encode([
+            'error' => 'Invalid quiz ID',
+            'description' => 'The provided quiz ID is not a number'
+        ]);
+        exit();
+    }
+
+    $quiz_id = $_GET['id']; // Assuming you want to fetch quiz with id 1
 
     // Fetch quiz information
     $quizStmt = $pdo->prepare("SELECT * FROM quizes WHERE id = :quiz_id");
     $quizStmt->execute(['quiz_id' => $quiz_id]);
     $quiz = $quizStmt->fetch(PDO::FETCH_ASSOC);
 
+    if (!$quiz) {
+        http_response_code(404);
+        echo json_encode(['error' => 'Quiz not found']);
+        exit();
+    }
+    
     // Fetch questions and options
     $questionsStmt = $pdo->prepare("SELECT * FROM questions WHERE quiz_id = :quiz_id");
     $questionsStmt->execute(['quiz_id' => $quiz_id]);
