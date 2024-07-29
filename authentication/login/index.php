17fd7c17e8e1c52220994ea37d7e3dfbbcad0c20
youssef <youssefhsn16@gmail.com>
2024-07-29 10:29:54 +0300

    💬 Login Now Accepts a message parameter

--- authentication/login/index.php
+++ authentication/login/index.php
@@ -5,15 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <?php 
-        $redirectTo = $_GET['destination']??"/quizlify/home/"; // this is for when someone tries to take a quiz but isn't logged in
+        $redirectTo = $_GET['greaterDestination']??"/quizlify/home/"; // this is for when someone tries to take a quiz but isn't logged in
     ?>
     <title>Login to your account</title>
 </head>
 <body>
     <div class="login">
         <h1>Test Yourself!</h1>
+        <p id="message"><?php echo $_GET['message'] ?? ''; ?></p>
         <form action="/quizlify/api/generateToken.php" method="post">
-            <input type="text" hidden name="destination" value="<?php echo $_GET['destination']; ?>">
+            <input type="text" hidden name="greaterDestination" value="<?php echo $_GET['greaterDestination']; ?>">
             <input type="text" name="username" placeholder="Username" required="required" />
             <input type="password" name="password" placeholder="Password" required="required" />
             <button type="submit" id="submit">Login</button>
