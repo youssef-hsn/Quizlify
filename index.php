ac4c35bce1ab9907d3078b24359cd836baa7973d
youssef <youssefhsn16@gmail.com>
2024-07-31 12:20:36 +0300

    🗑️ Remove unnecessary code

--- index.php
+++ index.php
@@ -2,14 +2,5 @@
 $request = $_SERVER['REQUEST_URI'];
 echo $request;
 $request = strtolower($_SERVER['REQUEST_URI']);
-switch ($request) {
-    case '/quizlify/':
-        header('Location: /quizlify/home/');
-    case '/login/':
-        header('Location: /quizlify/authentication/login/');
-    case '/register/':
-        header('Location: /quizlify/authentication/register/');
-    default:
-        header('Location: /quizlify/home/');
-}
+header('Location: /quizlify/home/');
 ?>
