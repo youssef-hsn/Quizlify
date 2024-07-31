b64ec2b3a124aa05edb26e9d3e1ab2b657615235
youssef <youssefhsn16@gmail.com>
2024-07-31 11:38:29 +0300

    ⌛ Add check for token expiration

--- api/whoAmI.php
+++ api/whoAmI.php
@@ -5,14 +5,14 @@ if (!isset($_COOKIE['token'])) {
 }
 if (!isset($_SESSION["user"])) {
    include "../config.php";
-   $sql = "SELECT id, username FROM users
+   $sql = "SELECT id, username, expires FROM users
            JOIN tokens ON tokens.user_id = users.id
            WHERE token = :token";
    $stmt = $pdo->prepare($sql);
    $stmt->bindParam(':token', $_COOKIE['token'], PDO::PARAM_STR);
    $stmt->execute();
    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-   if (!$result) {
+   if (!$result || $result['expires'] < time()) {   
         header("Location: /quizlify/authentication/login/?message=Found Invalid token in your cookies");
         exit();
     }
